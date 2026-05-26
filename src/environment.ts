4736e77c1d74bab87379316a4ecd2b08e7dabdef
Egor Koleda <radioegor146@ctf.by>
2026-05-26 05:20:47 +0300

    fix: zod types

--- src/environment.ts
+++ src/environment.ts
@@ -5,7 +5,7 @@ const environmentType = z.object({
   API_URLS: z.url().default('http://server:31116').transform(item => item.split(',')),
   MQTT_CA_CERTIFICATE_PATH: z.string().default('ca-cert.pem'),
   MQTT_TOPIC_PREFIX: z.string().default('bus/services/alice'),
-  MQTT_URL: z.url().default('mqtts://test:test@mqtt.int.bksp.in:8883'),
+  MQTT_URL: z.string().default('mqtts://test:test@mqtt.int.bksp.in:8883'),
 })
 
 export type Environment = z.infer<typeof environmentType>
