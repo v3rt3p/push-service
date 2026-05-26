6eb4b95bf786e6505ccc72c37b8977ddfa69494f
Egor Koleda <radioegor146@ctf.by>
2026-05-26 05:21:23 +0300

    fix

--- src/environment.ts
+++ src/environment.ts
@@ -2,10 +2,10 @@ import 'dotenv'
 import { z } from 'zod'
 
 const environmentType = z.object({
-  API_URLS: z.url().default('http://server:31116').transform(item => item.split(',')),
+  API_URLS: z.string().default('http://server:31116').transform(item => item.split(',')),
   MQTT_CA_CERTIFICATE_PATH: z.string().default('ca-cert.pem'),
   MQTT_TOPIC_PREFIX: z.string().default('bus/services/alice'),
-  MQTT_URL: z.string().default('mqtts://test:test@mqtt.int.bksp.in:8883'),
+  MQTT_URL: z.url().default('mqtts://test:test@mqtt.int.bksp.in:8883'),
 })
 
 export type Environment = z.infer<typeof environmentType>
